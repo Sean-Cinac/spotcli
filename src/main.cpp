252183ad1e9bd3fee7feab592c35728d396e31c2
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-10-30 14:17:25 +0100

    basic main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,42 @@
+#include <cstdint>
 #include <iostream>
 
-int main() { std::cout << "Hello World" << '\n'; }
+uint32_t askForChoice();
+void processChoice(uint32_t choice);
+void authSpotify();
+
+int main() {
+  std::cout << "spotcli main" << '\n';
+  uint32_t choice{askForChoice()};
+
+  processChoice(choice);
+}
+
+uint32_t askForChoice() {
+  uint32_t choice = {5};
+
+  while (choice > 4) {
+    std::cout << "You have these options:" << '\n'
+              << "auth: 0" << '\n'
+              << "play playlist: 1" << '\n'
+              << "play liked songs: 2" << '\n'
+              << "play artist: 3" << '\n'
+              << "play song: 4" << '\n';
+    if (!(std::cin >> choice)) {
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+      std::cout << "invalid input, 1-4" << '\n';
+    }
+  }
+  return choice;
+}
+
+void processChoice(uint32_t choice) {
+  switch (choice) {
+  case 0:
+    authSpotify();
+    break;
+  }
+}
+
+void authSpotify() {}
